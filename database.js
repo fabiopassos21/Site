f7598b6e3583eb820175748fe89c6b1bfaa4da03
fabiopassos21 <fabiopassos1997@gmail.com>
2024-06-29 18:13:21 -0300

    Site

--- database.js
+++ database.js
@@ -2,17 +2,25 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
-    // Cria tabela de usuários
+    // Cria tabela de usuários e insere dados
     db.run(`CREATE TABLE users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT
     )`);
 
-    // Insere usuários
-    const stmt = db.prepare("INSERT INTO users (name) VALUES (?)");
-    stmt.run("Fabio");
-    stmt.run("Leticia");
-    stmt.finalize();
+    const usersData = [
+        "Fabio", "Leticia", "Valeria", "Paulinho", "Bruno",
+        "Gaby", "Ademir", "Vinicius", "Felipe", "Neusa",
+        "Angelica", "Thainara", "Maria", "Felas", "Marcinho",
+        "Keila", "Samuel", "Sidney", "Pino", "Rosane",
+        "Simara", "Ailton", "Neli", "Roberto", "Diego"
+    ];
+
+    const stmtInsertUser = db.prepare("INSERT INTO users (name) VALUES (?)");
+    usersData.forEach(name => {
+        stmtInsertUser.run(name);
+    });
+    stmtInsertUser.finalize();
 
     // Cria tabela de seleções de presentes com coluna selected_by
     db.run(`CREATE TABLE gifts (
